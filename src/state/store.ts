c48f471facaadd33288676af74b819a42859d4bc
Vladislava <stdemidovez@gmail.com>
2024-08-25 20:07:21 +0300

    add app reducer + configure root reducer

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,19 +1,24 @@
 import {combineReducers, legacy_createStore} from "redux";
+import { appReducer } from './app-reducer'
+import { animalsReducer } from './animals-reducer'
+import { feedsReducer } from './feeds-reducer'
+import { culturesReducer } from './cultures-reducer'
+import { landResourcesReducer } from './landResources-reducer'
 
-//reducer-заглушка
-const dummyReducer = (state = {}, action: any) => {
-    return state;
-};
 
-// объединяя reducer-ы с помощью combineReducers,
-// мы задаём структуру нашего единственного объекта-состояния
 const rootReducer = combineReducers({
-    dummy: dummyReducer
+    app: appReducer,
+    animals: animalsReducer,
+    feeds: feedsReducer,
+    cultures: culturesReducer,
+    landResources: landResourcesReducer
 })
+
 // непосредственно создаём store
 export const store = legacy_createStore(rootReducer)
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
+
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
 window.store = store
