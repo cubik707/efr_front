8eadc97debd1b68ff291b250a04a5bfc4da7007a
Vladislava <stdemidovez@gmail.com>
2024-08-28 21:33:51 +0300

    output tableData for step 2

--- src/pages/outputData/tableOutputData.tsx
+++ src/pages/outputData/tableOutputData.tsx
@@ -1,6 +1,5 @@
 // @flow
-import {MenuItem, Select} from '@mui/material';
-import React from "react";
+import React from 'react'
 
 type TableCellData = string | number | React.ReactNode;
 export type TableRowData = TableCellData[];
@@ -19,29 +18,23 @@ export const tableData: tableDataItemType[] = [
     },
     {
         headers:
-            ['Культура','Урожайность прогнозная, ц/га ','в т.ч. на корм', 'в т.ч. на товар','в т.ч. на семена',],
+          ['Виды кормов',
+              'ц (Коров)',
+              'ц.к.ед (Коров)',
+              'ц (КРС)',
+              'ц.к.ед (КРС)',
+              'Структура кормов% (Коров)',
+              'Структура кормов% (КРС)',
+          ],
         rows: [
             [
-                <Select key="culture-select" defaultValue="" displayEmpty sx={{ minWidth: 271 }}>
-                    <MenuItem value="">
-                        <em>Выбрать культуру</em>
-                    </MenuItem>
-                    <MenuItem value="winterGrains">Зерновые озимые</MenuItem>
-                    <MenuItem value="springGrains">Зерновые яровые</MenuItem>
-                    <MenuItem value="pulses">Зернобобовые</MenuItem>
-                    <MenuItem value="rape">Рапс</MenuItem>
-                    <MenuItem value="hayGrassHay">Сено многолетних трав</MenuItem>
-                    <MenuItem value="haylageGrassHay">Сенаж многолетних трав</MenuItem>
-                    <MenuItem value="greenFodderGrassHay">Зеленый корм многолетних трав</MenuItem>
-                    <MenuItem value="hayImprovedHayfieldsAndPastures">Сено улучшенных сенокосов и пастбищ</MenuItem>
-                    <MenuItem value="haylageImprovedHayfieldsAndPastures">Сенаж улучшенных сенокосов и пастбищ</MenuItem>
-                    <MenuItem value="haylageNaturalHayfieldsAndPastures">Сенаж естественных сенокосов и пастбищ</MenuItem>
-                    <MenuItem value="greenFodderNaturalHayfieldsAndPastures">Зеленый корм естественных сенокосов и пастбищ</MenuItem>
-                </Select>,
-                'input',
-                'input',
-                'input',
-                'input'
+                'значения из B2',
+                'данные',
+                'данные',
+                'данные',
+                'данные',
+                'данные',
+                'данные',
             ]
         ],
     },
