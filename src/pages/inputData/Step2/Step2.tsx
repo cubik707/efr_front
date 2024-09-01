b63f4dba8411731e09ebe63e2ec8108d9f939283
Vladislava <stdemidovez@gmail.com>
2024-09-01 10:33:48 +0300

    add rows in Step2.tsx

--- src/pages/inputData/Step2/Step2.tsx
+++ src/pages/inputData/Step2/Step2.tsx
@@ -17,11 +17,28 @@ import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft'
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight'
 import { StepsProps } from '../Step1/Step1'
 import { AddBox } from '@mui/icons-material'
+import { useState } from 'react'
+import { CultureSelect } from './CultureSelect'
 import IndeterminateCheckBoxIcon from '@mui/icons-material/IndeterminateCheckBox'
 
 const headers = ['Культура', 'Урожайность прогнозная, ц/га ', 'в т.ч. на корм', 'в т.ч. на товар', 'в т.ч. на семена']
 
 export const Step2 = (props: StepsProps) => {
+  const [rows, setRows] = useState([{ culture: '', yield: '', fodder: '', commodity: '', seeds: '' }]);
+
+  const handleAddRow = () => {
+    setRows([...rows, { culture: '', yield: '', fodder: '', commodity: '', seeds: '' }]);
+  };
+
+  const handleDeleteRow = (index: number) => {
+    const newRows = rows.filter((_, rowIndex) => rowIndex !== index);
+    setRows(newRows);
+  };
+
+  const handleInputChange = (index: number, field: string, value: string) => {
+    const newRows = rows.map((row, rowIndex) => rowIndex === index ? { ...row, [field]: value } : row);
+    setRows(newRows);
+  };
   return (
     <Box sx={containerSx}>
       <TableContainer component={Paper}>
@@ -36,28 +53,53 @@ export const Step2 = (props: StepsProps) => {
             </TableRow>
           </TableHead>
           <TableBody>
-            <TableRow>
-              <TableCell>
-                <TextField
-                  fullWidth
-                />
-              </TableCell>
-              <TableCell>
-                <TextField
-                  fullWidth
-                />
-              </TableCell>
-              <TableCell>
-                <TextField
-                  fullWidth
-                />
-              </TableCell>
-              <TableCell>
-                <TextField
-                  fullWidth
-                />
-              </TableCell>
-            </TableRow>
+            {rows.map((row, rowIndex) => (
+              <TableRow key={rowIndex}>
+                <TableCell style={{display: 'flex', gap: '5px'}}>
+                  {rowIndex === rows.length - 1 ? (
+                    <IconButton color="primary" onClick={handleAddRow} size="small">
+                      <AddBox />
+                    </IconButton>
+                  ) : (
+                    <IconButton color="secondary" onClick={() => handleDeleteRow(rowIndex)} size="small">
+                      <IndeterminateCheckBoxIcon />
+                    </IconButton>
+                  )}
+                  <CultureSelect
+                    value={row.culture}
+                    onChange={(value) => handleInputChange(rowIndex, 'culture', value)}
+                  />
+                </TableCell>
+                <TableCell>
+                  <TextField
+                    value={row.yield}
+                    onChange={(e) => handleInputChange(rowIndex, 'yield', e.target.value)}
+                    fullWidth
+                  />
+                </TableCell>
+                <TableCell>
+                  <TextField
+                    value={row.fodder}
+                    onChange={(e) => handleInputChange(rowIndex, 'fodder', e.target.value)}
+                    fullWidth
+                  />
+                </TableCell>
+                <TableCell>
+                  <TextField
+                    value={row.commodity}
+                    onChange={(e) => handleInputChange(rowIndex, 'commodity', e.target.value)}
+                    fullWidth
+                  />
+                </TableCell>
+                <TableCell>
+                  <TextField
+                    value={row.seeds}
+                    onChange={(e) => handleInputChange(rowIndex, 'seeds', e.target.value)}
+                    fullWidth
+                  />
+                </TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
