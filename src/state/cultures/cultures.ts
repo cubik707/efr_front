d428cc53ed2bd5942bbdeddeacf3623db4a29128
Vladislava <stdemidovez@gmail.com>
2024-09-05 00:14:36 +0300

    change cultures

--- src/state/cultures/cultures.ts
+++ src/state/cultures/cultures.ts
@@ -6,18 +6,14 @@ export const cultures = {
   hayGrassHay: 'Сено многолетних трав',
   haylageGrassHay: 'Сенаж многолетних трав',
   greenFodderGrassHay: 'Зеленый корм многолетних трав',
-  hayfieldsOnHay: 'Сенокосы на сено',
-  hayfieldsOnSilage: 'Сенокосы на сенаж',
-  pasturesOnGreenFodder: 'Пастбища на зеленый корм',
-  pasturesOnSilage: 'Пастбища на сенаж',
+  hayImprovedHayfieldsAndPastures: 'Сено улучшенных сенокосов и пастбищ',
+  haylageImprovedHayfieldsAndPastures: 'Сенаж улучшенных сенокосов и пастбищ',
+  haylageNaturalHayfieldsAndPastures: 'Сенаж естественных сенокосов и пастбищ',
+  greenFodderNaturalHayfieldsAndPastures: 'Зеленый корм естественных сенокосов и пастбищ',
   annualGrasses: 'Однолетние травы на зеленый корм',
   cornOnSilage: 'Кукуруза на силос',
 } as const;
 
 export const unitedCultures = {
-  hayImprovedHayfieldsAndPastures: 'Сено улучшенных сенокосов и пастбищ',
-  haylageImprovedHayfieldsAndPastures: 'Сенаж улучшенных сенокосов и пастбищ',
-  haylageNaturalHayfieldsAndPastures: 'Сенаж естественных сенокосов и пастбищ',
-  greenFodderNaturalHayfieldsAndPastures: 'Зеленый корм естественных сенокосов и пастбищ',
   seeds: 'Зерно'
 } as const
