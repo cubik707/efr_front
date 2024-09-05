64e8926a7b4c441d616dbff6d8b967e5e5f89c59
Vladislava <stdemidovez@gmail.com>
2024-09-05 12:32:20 +0300

    output data in step6

--- src/pages/inputData/Step6/Step6.tsx
+++ src/pages/inputData/Step6/Step6.tsx
@@ -1,31 +1,72 @@
 // @flow
 import * as React from 'react'
-import { Box, Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+import {
+  Box,
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+} from '@mui/material'
 import { buttonContainerSx, containerSx, navigationButtonsContainerSx } from '../InputData.styles'
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft'
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight'
 import { StepsProps } from '../Step1/Step1'
 import { useFormik } from 'formik'
 import { validationSchema } from './step6-validation'
-import { useSelector } from 'react-redux'
 import { useAppSelector } from '../../../state/store'
+import { CultureNames } from '../../../state/cultures/cultures-reducer'
+import { cultures } from '../../../state/cultures/cultures'
 
 
 const headers = ['Вид продукции', 'Цена реализации за ц, руб.', 'Себестоимость за 1ц, руб.']
 
+const disabledPriceCultures = [
+  'hayGrassHay',
+  'haylageGrassHay',
+  'greenFodderGrassHay',
+  'hayImprovedHayfieldsAndPastures',
+  'haylageImprovedHayfieldsAndPastures',
+  'haylageNaturalHayfieldsAndPastures',
+  'greenFodderNaturalHayfieldsAndPastures',
+  'annualGrasses',
+  'cornOnSilage',
+]
 
+type RowType = {
+  sellingPricePerCent?: number | ''; // цена реализации продукции за ц
+  costPrice: number | ''; // себестоимость
+}
+
+type FormType = {
+  [k in CultureNames]: RowType
+}
 
 export const Step6 = (props: StepsProps) => {
-  const cultures = useAppSelector((state) => state.cultures);
+  const culturesState = useAppSelector((state) => state.cultures);
+  const cultureNames = Object.keys(culturesState).filter(culture => culture !== 'seeds') as CultureNames[];
 
-  const formik = useFormik<{}>({
-    initialValues: {  },
+  // Создание начального состояния для Formik
+  const initialValues: FormType = cultureNames.reduce((acc, culture) => {
+    acc[culture] = {
+      sellingPricePerCent: '',
+      costPrice: '',
+    };
+    return acc;
+  }, {} as FormType);
+
+  const formik = useFormik<FormType>({
+    initialValues,
     validationSchema,
     onSubmit: (values) => {
-      props.onNext()
+      props.onNext();
     },
-  })
-
+  });
+  console.log(cultureNames)
   return (
     <form style={{ width: '100%' }} onSubmit={formik.handleSubmit}>
       <Box sx={containerSx}>
@@ -41,21 +82,49 @@ export const Step6 = (props: StepsProps) => {
               </TableRow>
             </TableHead>
             <TableBody>
-
+              {cultureNames.map((cult:CultureNames, index) => (
+                <TableRow key={index}>
+                  <TableCell>{cultures[cult as keyof typeof cultures]}</TableCell>
+                  <TableCell>
+                    <TextField
+                      name={`${cult}.sellingPricePerCent`}
+                      value={formik.values[cult].sellingPricePerCent}
+                      onChange={(e) => formik.setFieldValue(`${cult}.sellingPricePerCent`, e.target.value)}
+                      fullWidth
+                      disabled={disabledPriceCultures.includes(cult)}
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <TextField
+                      name={`${cult}.costPrice`}
+                      value={formik.values[cult].costPrice}
+                      onChange={(e) => formik.setFieldValue(`${cult}.costPrice`, e.target.value)}
+                      fullWidth
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
             </TableBody>
           </Table>
         </TableContainer>
         <Box sx={buttonContainerSx}>
           <Button variant="outlined" disabled={true}>Найти оптимальные параметры</Button>
           <Box sx={navigationButtonsContainerSx}>
-            <Button variant="text"
-                    startIcon={<KeyboardArrowLeftIcon />}
-                    onClick={props.onBack}
-                    disabled={props.activeStep === 0}>Назад</Button>
-            <Button variant="contained"
-                    endIcon={<KeyboardArrowRightIcon />}
-                    onClick={props.onNext}
-                    disabled={false}>Далее</Button>
+            <Button
+              variant="text"
+              startIcon={<KeyboardArrowLeftIcon />}
+              onClick={props.onBack}
+              disabled={props.activeStep === 0}
+            >
+              Назад
+            </Button>
+            <Button
+              variant="contained"
+              endIcon={<KeyboardArrowRightIcon />}
+              type="submit"
+            >
+              Далее
+            </Button>
           </Box>
         </Box>
       </Box>
