6422d9726180265139a038a5092a292ba871faed
Vladislava <stdemidovez@gmail.com>
2024-09-05 22:53:52 +0300

    display data in output2

--- src/pages/outputData/Step2Output/Step2Output.tsx
+++ src/pages/outputData/Step2Output/Step2Output.tsx
@@ -1,5 +1,4 @@
 import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
-import { cultures } from '../../../state/cultures/cultures'
 import * as React from 'react'
 import { feedNamesInRussian } from '../../inputData/Step4/Step4'
 import { FeedName } from '../../../state/feeds/feeds-reducer'
@@ -17,6 +16,8 @@ const headers = ['Виды кормов',
 export const Step2Output = () => {
   // Получаем данные о кормах из состояния Redux
   const feedsState = useAppSelector(state => state.feeds);
+// Функция для округления значений
+  const formatNumber = (value: number) => value.toFixed(2);
 
   return (
     <TableContainer component={Paper}>
@@ -37,17 +38,17 @@ export const Step2Output = () => {
             return (
               <TableRow key={key}>
                 <TableCell>{feedName}</TableCell>
-                <TableCell>{feed.mainCows}</TableCell>
-                <TableCell>{feed.additionalCows}</TableCell>
-                <TableCell>{feed.mainYoungCattle}</TableCell>
-                <TableCell>{feed.additionalYoungCattle}</TableCell>
-                <TableCell></TableCell>
-                <TableCell></TableCell>
+                <TableCell>{formatNumber(feed.mainCows)}</TableCell>
+                <TableCell>{formatNumber(feed.additionalCows)}</TableCell>
+                <TableCell>{formatNumber(feed.mainYoungCattle)}</TableCell>
+                <TableCell>{formatNumber(feed.additionalYoungCattle)}</TableCell>
+                <TableCell>{/* Здесь добавьте логику для расчета и форматирования структуры кормов% (Коров) */}</TableCell>
+                <TableCell>{/* Здесь добавьте логику для расчета и форматирования структуры кормов% (КРС) */}</TableCell>
               </TableRow>
             );
           })}
         </TableBody>
       </Table>
     </TableContainer>
-  )
-}
+  );
+};
