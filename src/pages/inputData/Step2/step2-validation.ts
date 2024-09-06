6e037330edfb5743142b6cfe6082d054d867de8c
Vladislava <stdemidovez@gmail.com>
2024-09-06 12:40:01 +0300

    validation with rape

--- src/pages/inputData/Step2/step2-validation.ts
+++ src/pages/inputData/Step2/step2-validation.ts
@@ -1,14 +1,23 @@
 import * as Yup from 'yup'
 import { commonValidationSchema } from '../../../utils/commonValidationsSchema'
+import { CultureNames } from '../../../state/cultures/cultures-reducer'
 
 export const validationSchema = Yup.object().shape({
   rows: Yup.array().of(
     Yup.object({
       culture: Yup.string().required('Выберите культуру'),
       yield: commonValidationSchema,
-      fodder: commonValidationSchema.test('fodder-not-exceeds-yield', 'Значение на корм не может превышать урожайность прогнозную', function (value) {
-        const { yield: forecastYield, fodder } = this.parent;
-        return !fodder || !forecastYield || Number(fodder) <= Number(forecastYield);
+      fodder: Yup.mixed().when('culture', (culture: CultureNames[], schema: Yup.MixedSchema) => {
+        return culture.includes('rape') // Проверка наличия 'rape' в массиве
+          ? schema.nullable() // Делает поле необязательным для "rape"
+          : commonValidationSchema.test(
+            'fodder-not-exceeds-yield',
+            'Значение на корм не может превышать урожайность прогнозную',
+            function (value) {
+              const { yield: forecastYield, fodder } = this.parent;
+              return !fodder || !forecastYield || Number(fodder) <= Number(forecastYield);
+            }
+          );
       }),
     })
   )
