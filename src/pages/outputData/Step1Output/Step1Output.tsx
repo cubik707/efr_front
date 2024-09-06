311847c1a528ad8ce1b8e5ed24fa67f7da55ca0e
Vladislava <stdemidovez@gmail.com>
2024-09-06 15:39:00 +0300

    calculatePercentage

--- src/pages/outputData/Step1Output/Step1Output.tsx
+++ src/pages/outputData/Step1Output/Step1Output.tsx
@@ -1,18 +1,36 @@
 // @flow
-import * as React from 'react'
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
-import { cultures } from '../../../state/cultures/cultures'
-import { useAppSelector } from '../../../state/store'
+import * as React from 'react';
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { cultures } from '../../../state/cultures/cultures';
+import { useAppSelector } from '../../../state/store';
 
-type Props = {
+// Типы данных
+type Props = {};
 
-};
+// Заголовки таблицы
+const headers = ['Культуры', 'Расчетное значение площадь', 'Процентное соотношение площадь'];
 
-const headers = ['Культуры', 'Расчетное значение площадь', 'Расчетное значение площадь %']
-
+// Компонент
 export const Step1Output = (props: Props) => {
-  // Получаем данные о культурах из состояния Redux
+  // Получаем данные о культурах и ресурсах земли из состояния Redux
   const culturesState = useAppSelector(state => state.cultures);
+  const landResources = useAppSelector(state => state.landResources);
+
+  // Расчет процентного соотношения
+  const calculatePercentage = (cultureKey: string) => {
+    const cultureSquare = culturesState[cultureKey as keyof typeof cultures]?.square || 0;
+    const totalSquare = landResources.arableLand;
+
+    if (cultureKey === 'hayImprovedHayfieldsAndPastures' || cultureKey === 'haylageImprovedHayfieldsAndPastures') {
+      const improvedTotal = landResources.hayfieldsAndPastureImproved;
+      return (cultureSquare / improvedTotal) * 100;
+    } else if (cultureKey === 'haylageNaturalHayfieldsAndPastures' || cultureKey === 'greenFodderNaturalHayfieldsAndPastures') {
+      const naturalTotal = landResources.hayfieldsAndPastureNatural;
+      return (cultureSquare / naturalTotal) * 100;
+    } else {
+      return (cultureSquare / totalSquare) * 100;
+    }
+  };
 
   return (
     <TableContainer component={Paper}>
@@ -31,7 +49,7 @@ export const Step1Output = (props: Props) => {
             <TableRow key={key}>
               <TableCell>{label}</TableCell>
               <TableCell>{Math.round(culturesState[key as keyof typeof cultures].square || 0)}</TableCell>
-              <TableCell></TableCell>
+              <TableCell>{Math.round(calculatePercentage(key))}%</TableCell>
             </TableRow>
           ))}
         </TableBody>
